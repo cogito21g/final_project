fc1bdceee3e63677dd5bb3299c436e872470bf88
ChoHyoungSeo <francesco.johs@gmail.com>
2024-02-27 22:00:14 +0900

    init FastAPI

---
+++ app.py
@@ -0,0 +1,31 @@
+from typing import Union
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+
+templates = Jinja2Templates(directory="templates")
+app = FastAPI()
+
+@app.get("/")
+async def home(request:Request):
+	return templates.TemplateResponse("index.html", {'request': request})
+
+@app.get('/login')
+async def login(request:Request):
+	return templates.TemplateResponse('login.html',{'request': request})
+
+@app.get('/signup')
+async def login(request:Request):
+	return templates.TemplateResponse('signup.html',{'request': request})
+
+@app.get('/album')
+async def login(request:Request):
+	return templates.TemplateResponse('album.html',{'request': request})
+
+@app.get('/upload')
+async def login(request:Request):
+	return templates.TemplateResponse('upload.html',{'request': request})
+
+if __name__ == '__main__':
+	uvicorn.run(app, host='0.0.0.0', port=8000)
