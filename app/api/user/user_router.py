cb1393c5ce46f63e9a23c8754aa7cfccdef8b994
root <pjm14418@gmail.com>
2024-03-03 18:37:29 +0900

    feat: change user_create func

--- app/api/user/user_router.py
+++ app/api/user/user_router.py
@@ -1,7 +1,8 @@
 from datetime import timedelta, datetime
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
@@ -9,7 +10,8 @@ from sqlalchemy.orm import Session
 from starlette import status
 
 from core.config import get_settings
-from db.database import get_db
+from db.database import get_db, db_engine
+from models import models
 from crud import crud
 from crud.crud import pwd_context
 from schemas import schemas
@@ -18,43 +20,61 @@ settings = get_settings()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-router = APIRouter()
+router = APIRouter(prefix='/login')
+templates = Jinja2Templates(directory="templates")
 
 
-@router.post("/signup", status_code=status.HTTP_204_NO_CONTENT)
-def user_create(_user_create: schemas.UserCreate, db: Session = Depends(get_db)):
-    user = crud.get_existing_user(db, user_create=_user_create)
-    if user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="이미 존재하는 사용자입니다.")
-    crud.create_user(db=db, user=_user_create)
+@router.post('')
+async def user_create(request: Request):
+    body = await request.form()     # html에서 받은 form 데이터
 
 
-@router.post("/login", response_model=schemas.Token)
-def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
-                           db: Session = Depends(get_db)):
+    with Session(db_engine) as session:
+        # User 테이블에 입력한 이메일이 있는지 확인 -> 있다면 회원가입 화면 다시 띄우기
+        user = session.query(models.User).filter(models.User.email == body['email']).first()
+        if user:
+            return RedirectResponse(url='/signup')
+        
+        # 입력 받은 비밀번호 2개가 같은지 확인 -> 다르다면 회원가입 화면 다시 띄우기
+        if body['pw'] != body['check_pw']:
+            return RedirectResponse(url='/signup')
+        
+        # 입력받은 값들을 User 테이블에 저장
+        user_info = models.User(email = body['email'],
+                                password = pwd_context.hash(body['pw']))
+        
+        session.add(user_info)
+        session.commit()
+        session.refresh(user_info)
+    
+    return templates.TemplateResponse("login.html", {"request": request, "body": body})
 
-    # check user and password
-    user = crud.get_user_by_email(db, form_data.username)
-    if not user or not pwd_context.verify(form_data.password, user.password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="이메일 혹은 비밀번호가 올바르지 않습니다.",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
 
-    # make access token
-    data = {
-        "sub": user.email,
-        "exp": datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    }
-    access_token = jwt.encode(data, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+# @router.post("/", response_model=schemas.Token)
+# def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+#                            db: Session = Depends(get_db)):
 
-    return {
-        "access_token": access_token,
-        "token_type": "bearer",
-        "email": user.email
-    }
+#     # check user and password
+#     user = crud.get_user_by_email(db, form_data.username)
+#     if not user or not pwd_context.verify(form_data.password, user.password):
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="이메일 혹은 비밀번호가 올바르지 않습니다.",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+
+#     # make access token
+#     data = {
+#         "sub": user.email,
+#         "exp": datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+#     }
+#     access_token = jwt.encode(data, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+#     return {
+#         "access_token": access_token,
+#         "token_type": "bearer",
+#         "email": user.email
+#     }
 
 
 def get_current_user(token: str = Depends(oauth2_scheme),
