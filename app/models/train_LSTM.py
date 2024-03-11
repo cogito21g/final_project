8f12b6e7bc8d0411bf41c5f8267921f82ac60a77
KimSiwoong <pao.kim.si.woong@gmail.com>
2024-03-11 15:54:04 +0900

    [feat] feature_extraction 코드 수정 + abnormal dataset 추가

--- app/models/train_LSTM.py
+++ app/models/train_LSTM.py
@@ -23,184 +23,10 @@ import os.path as osp
 
 from tqdm import tqdm
 
+import wandb
 
-class NormalDataset(Dataset):
-
-    def __init__(
-        self,
-        sequence_length=20,
-        prediction_time=1,
-        root="/data/ephemeral/home/level2-3-cv-finalproject-cv-06/datapreprocess/csv",
-    ):
-        super().__init__()
-        self.sequence_length = sequence_length
-        self.prediction_time = prediction_time
-
-        self.scaler = MinMaxScaler()
-        # 데이터 값 [0,1] 범위로 scaling할때 사용
-
-        # Load the dataset
-        file_list = os.listdir(root)
-
-        df_list = []
-
-        self.length = 0
-        self.range_table = []
-
-        self.real_length = 0
-        self.real_idx_table = []
-
-        for i, file_name in enumerate(file_list):
-            dat = pd.read_csv(root + "/" + file_name)
-            dat.drop(columns=["Frame"], inplace=True)  # Remove the 'Frame' column
-
-            print(f"==>>{i}번째 dat.shape: {dat.shape}")
-
-            id_counter = pd.Series(dat["ID"]).value_counts(sort=False)
-
-            for id_to_del in id_counter[id_counter < sequence_length + prediction_time].index:
-                dat.drop(dat[dat["ID"] == id_to_del].index, inplace=True)
-
-            id_counter = pd.Series(dat["ID"]).value_counts(sort=False)
-
-            print(f"==>>{i}번째 처리 후 dat.shape: {dat.shape}")
-            assert len(id_counter[id_counter < sequence_length + prediction_time].index) == 0
-
-            for count in id_counter:
-                cur_id_length = count - sequence_length - prediction_time + 1
-                self.range_table.append(self.length + cur_id_length)
-                self.real_idx_table.append(self.real_length + count)
-                self.length += cur_id_length
-                self.real_length += count
-
-            dat["ID"] = dat["ID"].astype("str") + f"_{i}"
-            df_list.append(dat.copy())
-
-        self.dat = pd.concat(df_list, ignore_index=True)
-        # self.dat.drop(columns=["Frame"], inplace=True)  # Remove the 'Frame' column
-
-        id_counter = pd.Series(self.dat["ID"]).value_counts(sort=False)
-
-        # # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        # DF를 다 합치고 나서 ID를 거르면 데이터셋 초기화에 1분정도 걸리지만
-        # DF 조각마다 ID를 거르고나서 합치면 6초 밖에 안 걸린다
-        # self.checker = []
-
-        # for id_to_del in id_counter[id_counter < sequence_length + prediction_time].index:
-        #     self.checker.append((id_to_del, id_counter[id_to_del]))
-
-        #     self.dat.drop(self.dat[self.dat["ID"] == id_to_del].index, inplace=True)
-
-        # # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        # # sequence_length + prediction_time 보다 짧은 ID를 지우는 것을 한번만 하면
-        # # 13개 ID가 sequence_length + prediction_time보다 짧은데도 남아 있다???
-        # id_counter = pd.Series(self.dat["ID"]).value_counts(sort=False)
-
-        # if len(id_counter[id_counter < sequence_length + prediction_time].index) != 0:
-        #     for id_to_del in id_counter[id_counter < sequence_length + prediction_time].index:
-        #         self.checker.append((id_to_del, id_counter[id_to_del]))
-
-        #         self.dat.drop(self.dat[self.dat["ID"] == id_to_del].index, inplace=True)
-
-        # id_counter = pd.Series(self.dat["ID"]).value_counts(sort=False)
-        # # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-        assert len(id_counter[id_counter < sequence_length + prediction_time].index) == 0
-
-        # self.length = 0
-
-        # self.range_table = []
-
-        # for count in id_counter:
-        #     cur_id_length = count - sequence_length - prediction_time + 1
-        #     self.range_table.append(self.length + cur_id_length)
-        #     self.length += cur_id_length
-
-        # self.dat.drop(columns=["ID"], inplace=True)
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, idx):
-        real_idx = self.find_real_idx(idx)
-
-        sequence = self.dat[real_idx : real_idx + self.sequence_length].copy()
-        sequence.drop(columns=["ID"], inplace=True)
-        sequence = np.array(sequence)
-        # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        # sequence = self.scaler.fit_transform(sequence.values)
-        # # 데이터 값 [min, max] -> [0,1] 범위로 scaling
-        # scale 된 후에는 numpy array로 변환된다
-        # sequence나 target은 이미 yolo v8에서 xywhn, xyn으로 0~1 범위인데 scaling을 한번 더 할 필요가 있을지?
-        # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        # (self.sequence_length, 38)
-        target = self.dat[
-            real_idx + self.sequence_length : real_idx + self.sequence_length + self.prediction_time
-        ].copy()
-        target.drop(columns=["ID"], inplace=True)
-        target = np.array(target)
-        # target = self.scaler.fit_transform(target.values)
-        # (self.prediction_time, 38)
-
-        return torch.from_numpy(sequence).float(), torch.from_numpy(target).float()
-
-    def find_real_idx(self, idx):
-
-        start = 0
-        end = len(self.range_table) - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if self.range_table[mid] == idx:
-                real_idx = idx + ((mid + 1) * (self.sequence_length + self.prediction_time - 1))
-                return real_idx
-
-            if self.range_table[mid] > idx:
-                end = mid - 1
-            else:
-                start = mid + 1
-
-        real_idx = idx + (start * (self.sequence_length + self.prediction_time - 1))
-
-        return real_idx
-
-
-class LSTMAutoencoder(nn.Module):
-    def __init__(self, sequence_length, n_features, prediction_time):
-        super(LSTMAutoencoder, self).__init__()
-
-        self.sequence_length = sequence_length
-        self.n_features = n_features
-        self.prediction_time = prediction_time
-
-        # Encoder
-        self.encoder = nn.LSTM(input_size=n_features, hidden_size=100, batch_first=True)
-        self.encoder2 = nn.LSTM(input_size=100, hidden_size=50, batch_first=True)
-
-        # Repeat vector for prediction_time
-        self.repeat_vector = nn.Sequential(
-            nn.ReplicationPad1d(padding=(0, prediction_time - 1)),
-            nn.ReplicationPad1d(padding=(0, 0)),  # Adjusted padding
-        )
-
-        # Decoder
-        self.decoder = nn.LSTM(input_size=50, hidden_size=100, batch_first=True)
-        self.decoder2 = nn.LSTM(input_size=100, hidden_size=n_features, batch_first=True)
-
-    def forward(self, x):
-        # Encoder
-        # _, (x, _) = self.encoder(x)
-        x, (_, _) = self.encoder(x)
-        # output, (hn, cn) = rnn(x)
-        x, (_, _) = self.encoder2(x)
-
-        # Repeat vector for prediction_time
-        x = self.repeat_vector(x)
-
-        # Decoder
-        x, (_, _) = self.decoder(x)
-        x, (_, _) = self.decoder2(x)
-
-        return x[:, -1, :].unsqueeze(dim=1)
+from shop_dataset import NormalDataset, AbnormalDataset
+from classifier import LSTMAutoencoder
 
 
 def parse_args():
@@ -211,10 +37,28 @@ def parse_args():
         "--root_dir",
         type=str,
         default=os.environ.get(
-            "SM_CHANNEL_TRAIN_CSV", "/data/ephemeral/home/level2-3-cv-finalproject-cv-06/datapreprocess/csv"
+            "SM_CHANNEL_TRAIN_CSV",
+            "/data/ephemeral/home/level2-3-cv-finalproject-cv-06/datapreprocess/csv/normal/val",
         ),
     )
     # 학습 데이터 경로
+    parser.add_argument(
+        "--abnormal_root_dir",
+        type=str,
+        default=os.environ.get(
+            "SM_CHANNEL_ABNORMAL_CSV",
+            "/data/ephemeral/home/level2-3-cv-finalproject-cv-06/datapreprocess/csv/abnormal/val",
+        ),
+    )
+    parser.add_argument(
+        "--json_dir",
+        type=str,
+        default=os.environ.get(
+            "SM_CHANNEL_ABNORMAL_JSON",
+            "/data/ephemeral/home/level2-3-cv-finalproject-cv-06/datapreprocess/json/abnormal/val",
+        ),
+    )
+    # json파일 경로
     parser.add_argument(
         "--model_dir", type=str, default=os.environ.get("SM_MODEL_DIR", "/data/ephemeral/home/pths")
     )
@@ -236,7 +80,7 @@ def parse_args():
     parser.add_argument("--val_batch_size", type=int, default=64)
     parser.add_argument("--val_num_workers", type=int, default=8)
     parser.add_argument("--learning_rate", type=float, default=0.001)
-    parser.add_argument("--max_epoch", type=int, default=20)
+    parser.add_argument("--max_epoch", type=int, default=50)
 
     parser.add_argument("--save_interval", type=int, default=1)
     parser.add_argument("--val_interval", type=int, default=1)
@@ -270,6 +114,8 @@ def set_seed(seed):
 
 def train(
     root_dir,
+    abnormal_root_dir,
+    json_dir,
     model_dir,
     model_name,
     device,
@@ -329,6 +175,15 @@ def train(
         dataset=valid_dataset, batch_size=val_batch_size, shuffle=False, num_workers=val_num_workers
     )
 
+    abnormal_dataset = AbnormalDataset(
+        root=abnormal_root_dir,
+        label_root=json_dir,
+    )
+
+    abnormal_loader = DataLoader(
+        dataset=abnormal_dataset, batch_size=val_batch_size, shuffle=False, num_workers=val_num_workers
+    )
+
     data_load_end = datetime.now()
     data_load_time = data_load_end - time_start
     data_load_time = str(data_load_time).split(".")[0]
@@ -343,17 +198,17 @@ def train(
     #     load_dict = torch.load(osp.join(model_dir, f"{resume_name}.pth"), map_location="cpu")
     #     model.load_state_dict(load_dict["model_state_dict"])
 
-    model.load_state_dict(
-        torch.load(
-            "/data/ephemeral/home/level2-3-cv-finalproject-cv-06/app/models/pytorch_model.pth",
-            map_location="cpu",
-        )
-    )
+    # model.load_state_dict(
+    #     torch.load(
+    #         "/data/ephemeral/home/level2-3-cv-finalproject-cv-06/app/models/pytorch_model.pth",
+    #         map_location="cpu",
+    #     )
+    # )
     model.to(device)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-6)
 
-    # scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[15, 40], gamma=0.1)
 
     # if resume_name:
     #     optimizer.load_state_dict(load_dict["optimizer_state_dict"])
@@ -361,24 +216,25 @@ def train(
     #     scaler.load_state_dict(load_dict["scaler_state_dict"])
 
     criterion = nn.MSELoss()
+    val_criterion = nn.MSELoss(reduction="none")
 
     print(f"Start training..")
 
-    # wandb.init(
-    #     project="VAD",
-    #     entity="pao-kim-si-woong",
-    #     config={
-    #         "lr": learning_rate,
-    #         "dataset": "무인매장",
-    #         "n_epochs": max_epoch,
-    #         "loss": "MSE",
-    #         "notes": "VAD 실험",
-    #     },
-    #     name=wandb_run_name + "_" + train_start,
-    #     mode=wandb_mode,
-    # )
+    wandb.init(
+        project="VAD",
+        entity="pao-kim-si-woong",
+        config={
+            "lr": learning_rate,
+            "dataset": "무인매장",
+            "n_epochs": max_epoch,
+            "loss": "MSE",
+            "notes": "VAD 실험",
+        },
+        name=wandb_run_name + "_" + train_start,
+        mode=wandb_mode,
+    )
 
-    # wandb.watch((model,))
+    wandb.watch((model,))
 
     best_loss = np.inf
 
@@ -391,7 +247,7 @@ def train(
 
         epoch_loss = 0
 
-        for step, (x, y) in tqdm(enumerate(train_loader), total=total_batches):
+        for step, (x, y, _) in tqdm(enumerate(train_loader), total=total_batches):
 
             x, y = x.to(device), y.to(device)
 
@@ -422,7 +278,7 @@ def train(
                 "model_name": model_name,
                 "model_state_dict": model.state_dict(),  # 모델의 state_dict 저장
                 "optimizer_state_dict": optimizer.state_dict(),
-                # "scheduler_state_dict": scheduler.state_dict(),
+                "scheduler_state_dict": scheduler.state_dict(),
                 # "scaler_state_dict": scaler.state_dict(),
             }
 
@@ -436,8 +292,9 @@ def train(
 
             with torch.no_grad():
                 total_loss = 0
+                total_abnormal_loss = 0
 
-                for step, (x, y) in tqdm(enumerate(valid_loader), total=len(valid_loader)):
+                for step, (x, y, label) in tqdm(enumerate(valid_loader), total=len(valid_loader)):
                     x, y = x.to(device), y.to(device)
 
                     pred = model(x)
@@ -468,21 +325,15 @@ def train(
             else:
                 counter += 1
 
-        # new_wandb_metric_dict = {
-        #     "valid_dice": avg_dice,
-        #     "valid_loss": val_mean_loss,
-        #     "valid_f_loss": val_mean_f_loss,
-        #     "train_bce_loss": epoch_mean_b_loss,
-        #     "train_focal_loss": epoch_mean_f_loss,
-        #     "train_dice_loss": epoch_mean_d_loss,
-        #     # "train_loss": epoch_mean_loss,
-        #     "train_loss": epoch_mean_b_loss + epoch_mean_d_loss,
-        #     "learning_rate": scheduler.get_lr()[0],
-        # }
+        new_wandb_metric_dict = {
+            "valid_loss": val_mean_loss,
+            "train_loss": epoch_mean_loss,
+            "learning_rate": scheduler.get_lr()[0],
+        }
 
-        # wandb.log(new_wandb_metric_dict)
+        wandb.log(new_wandb_metric_dict)
 
-        # scheduler.step()
+        scheduler.step()
 
         epoch_end = datetime.now()
         epoch_time = epoch_end - epoch_start
