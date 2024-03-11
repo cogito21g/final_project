d83d11171bc0232648566043379db6ed2642dbc2
sujin1013 <popo2419@naver.com>
2024-03-11 16:23:34 +0900

    [feat] feature extraction python file

---
+++ datapreprocess/run.sh
@@ -0,0 +1,18 @@
+# python feature_extraction.py --name Abandonment
+# python feature_extraction_gpu.py --name Arson
+# python feature_extraction.py --name Assault ;
+# python feature_extraction.py --name Conductivity ;
+# python feature_extraction.py --name Damage ;
+# python feature_extraction.py --name Smoking ;
+# python feature_extraction.py --name Theft ;
+# python feature_extraction.py --name Weak ;
+
+
+python feature_extraction_videoMAEv2.py --name Abandonment;
+python feature_extraction_videoMAEv2.py --name Arson;
+python feature_extraction_videoMAEv2.py --name Assault ;
+python feature_extraction_videoMAEv2.py --name Conductivity ;
+python feature_extraction_videoMAEv2.py --name Damage ;
+python feature_extraction_videoMAEv2.py --name Smoking ;
+python feature_extraction_videoMAEv2.py --name Theft ;
+python feature_extraction_videoMAEv2.py --name Weak ;
